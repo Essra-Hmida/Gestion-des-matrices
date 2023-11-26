bcd531d2929e650acab4a78a296d9bce334e40e0
Essra-Hmida <hmidaesraa@gmail.com>
2023-11-26 17:12:54 +0100

    interface

--- Gestion-des-matrices.py
+++ Gestion-des-matrices.py
@@ -1,11 +1,34 @@
 from tkinter import *
 import tkinter
+import random
+from PIL import Image, ImageTk
+
 
 fenetre = Tk()
-fenetre.geometry('700x200')
+
 fenetre.title('Gestion des matrices')
 fenetre['bg']='#F0EAD2'
-label=Label (fenetre,text="Choisir un traiment",background='#F0EAD2',font=("Times New Roman",20,"italic bold")).pack()
+#fenetre.iconbitmap('M.ico')
+
+image_path = r"C:\Users\essra hmida\Downloads\istockphoto-1457115338-612x612 (5).jpg"  # Use a raw string
+original_image = Image.open(image_path)
+converted_image = ImageTk.PhotoImage(original_image)
+# Get the width and height of the image
+image_width = converted_image.width()
+image_height = converted_image.height()
+
+# Set the window size to match the image size
+fenetre.geometry(f"{image_width}x{image_height}")
+# Create a label to hold the image
+background_label = Label(fenetre, image=converted_image)
+background_label.place(relwidth=1, relheight=1)
+label=Label (fenetre,text="Gestion des matrices \n  Bienvenue!On est ici pour t'aider à faire des divers  opérations sur les matrices \n  n'hésitez pas de choisir l'opréation que vous voulez faire",font=("Times New Roman",15,"italic bold"),relief="raised",highlightthickness=6 , borderwidth=5)
+label.pack()
+label.place(x=150,y=20)
+    
+ 
+    
+
 
 
     
@@ -33,30 +56,9 @@ def passer_a_matrice_x_vecteur():
     nouvelle_fenetre.geometry('700x700')
     nouvelle_fenetre.title('matrice x vecteur')
     nouvelle_fenetre['bg'] = 'mistyrose1'
-    def saisie():
-        label=Label (nouvelle_fenetre,text="multiplication",background='mistyrose1',font=("Times New Roman",20,"italic ")).pack()
+    
 
-    # Ajoutez les éléments de la nouvelle fenêtre ici
-    menu1=Menu(nouvelle_fenetre)
-     #sous onglet
-    les_types=Menu(menu1,background='mistyrose1')
-    les_types.add_command(label="dense", command=saisie, background='darksalmon')
-    les_types.add_command(label="bande", command=saisie, background='darksalmon')
-    les_types.add_command(label="demi_bande", command=saisie, background='darksalmon')
-    les_types.add_command(label="symetrique", command=saisie, background='darksalmon')
-    les_types.add_command(label="triangulaire superieure", command=saisie, background='darksalmon')
-    les_types.add_command(label="triangulaire inferieure", command=saisie, background='darksalmon')
-    les_types.add_command(label="demi triangulaire inferieure", command=saisie, background='darksalmon')
-    les_types.add_command(label="demi triangulaire superieure", command=saisie, background='darksalmon')
-
-
-
-
-     #principles
-    menu1.add_cascade(label="les types",menu=les_types,background='pink')
-
-    # Associez le menu à la fenêtre principale
-    nouvelle_fenetre.config(menu=menu1)
+    
     nouvelle_fenetre.mainloop()
    
 
@@ -66,61 +68,25 @@ def passer_a_matrice_x_matrice():
 
     # Créer et afficher une nouvelle fenêtre de résolution
     nouvelle_fenetre = Tk()
-    nouvelle_fenetre.geometry('700x700')
+    nouvelle_fenetre.geometry('800x800')
     nouvelle_fenetre.title('matrice x matrice')
-    nouvelle_fenetre['bg'] = 'mistyrose1'
-    def saisie():
-        label=Label (nouvelle_fenetre,text="multiplication",background='mistyrose1',font=("Times New Roman",20,"italic bold")).pack()
+    nouvelle_fenetre['bg'] = '#BCD2EE'
+    label=Label (nouvelle_fenetre,text="Choisir le type de votre matrice",font=("Times New Roman",15,"italic bold"),relief="raised",highlightthickness=6 , borderwidth=5)
+    label.pack()
+    label.place(x=250,y=15)
+   
 
-    # Ajoutez les éléments de la nouvelle fenêtre ici
-
-     
-    menu2=Menu(nouvelle_fenetre)
-    #sous onglet
-    les_types=Menu(menu2,background='mistyrose1')
-    les_types.add_command(label="dense x dense", command=saisie, background='darksalmon')
-    les_types.add_command(label="dense x transpose", command=saisie, background='darksalmon')
-    les_types.add_command(label="bande x bande", command=saisie, background='darksalmon')
-    les_types.add_command(label="dense x bande", command=saisie, background='darksalmon')
-    les_types.add_command(label="bande x dense", command=saisie, background='darksalmon')
-    les_types.add_command(label="dense x demi_bande", command=saisie, background='darksalmon')
-    les_types.add_command(label="demi_bande x dense", command=saisie, background='darksalmon')
-    les_types.add_command(label="dense xtriangulaire inferieure ", command=saisie, background='darksalmon')
-    les_types.add_command(label="dense xtriangulaire superieure ", command=saisie, background='darksalmon')
-    les_types.add_command(label="bande xtriangulaire inferieure ", command=saisie, background='darksalmon')
-    les_types.add_command(label="bande xtriangulaire superieure ", command=saisie, background='darksalmon')
-    les_types.add_command(label="triangulaire inferieure x triangulaire inferieure", command=saisie, background='darksalmon')
-    les_types.add_command(label="triangulaire superieure x triangulaire superieure", command=saisie, background='darksalmon')
-    les_types.add_command(label="triangulaire inferieure x triangulaire superieure", command=saisie, background='darksalmon')
-    les_types.add_command(label="triangulaire superieure x triangulaire inferieure", command=saisie, background='darksalmon')
-    les_types.add_command(label="demi triangulaire superieure x demi triangulaire inferieure", command=saisie, background='darksalmon')
-
-    #principles
-    menu2.add_cascade(label="les types",menu=les_types,background='pink')
-  
-
-    # Associez le menu à la fenêtre principale
-    nouvelle_fenetre.config(menu=menu2)
     nouvelle_fenetre.mainloop()
 
 
-mon_menu=Menu(fenetre)
-#sous onglet matrice x vecteur
-matrice_x_vecteur=Menu(mon_menu)
+button=Button(text="Multiplication matrice × vecteur",command=passer_a_matrice_x_vecteur,width=30,height= 2, relief='raised', highlightthickness=6 , borderwidth=5,font=("Times New Roman",15,"italic bold"))
+button.pack()
+button.place(x=600, y=200)
+button=Button(text="Multiplication matrice × matrice",command=passer_a_matrice_x_matrice,width=30,height= 2, relief='raised', highlightthickness=6 , borderwidth=5,font=("Times New Roman",15,"italic bold"))
+button.pack()
+button.place(x=600, y=300) 
+button=Button(text="Résolution",command=passer_a_resolution,width=30,height= 2, relief='raised', highlightthickness=6 , borderwidth=5,font=("Times New Roman",15,"italic bold"))
+button.pack()
+button.place(x=600, y=400)  
 
-#sous onglet matrice x matrice
-matrice_x_matrice=Menu(mon_menu)
-#sous onglet multiplication
-Multiplication=Menu(mon_menu,background='#A98467',tearoff=0)
-Multiplication.add_command(label="passer à la matrice x vecteur",command=passer_a_matrice_x_vecteur,background='#DDE5B6')
-Multiplication.add_command(label="passer à la matrice x matrice",command=passer_a_matrice_x_matrice,background='#DDE5B6')
-#sous onglet resolution
-resolution=Menu(mon_menu,background='#ADC178',tearoff=0)
-resolution.add_command(label="Passer à la résolution", command=passer_a_resolution, background='#A98467')
-#principles
-mon_menu.add_cascade(label="Multiplication",menu=Multiplication,background='#ADC178')
-mon_menu.add_cascade(label="Résolution", menu=resolution, background='#A98467')
-
-# Associez le menu à la fenêtre principale
-fenetre.config(menu=mon_menu)
 fenetre.mainloop()
